a69086a872ac2820abae83844c07548c8d4c2251
bpolitiadis <36727945+bpolitiadis@users.noreply.github.com>
2022-09-24 00:24:49 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import ManualHeader from "../components/ManualHeader";
 import Header from "../components/Header";
-import LotteryEntrance from "../components/lotteryEntrance";
+import LotteryEntrance from "../components/LotteryEntrance";
 
 export default function Home() {
     return (
