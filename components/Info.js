c55bb1f41bd0449738214665c2959b9e65831894
bpolitiadis <b.politiadis@gmail.com>
2022-09-28 17:14:23 +0000

    FE v1

--- components/Info.js
+++ components/Info.js
@@ -7,12 +7,9 @@ import {
     useNativeBalance,
 } from "react-moralis";
 import { abi, contractAddresses } from "../constants";
-import { ethers } from "ethers";
 
 export default function Info() {
-
     const { Moralis, isWeb3Enabled, chainId: chainIdHex } = useMoralis();
-    // These get re-rendered every time due to our connect button!
     const chainId = parseInt(chainIdHex);
     const deKinoAddress = chainId in contractAddresses ? contractAddresses[chainId][0] : null;
 
@@ -21,7 +18,6 @@ export default function Info() {
     const [numberOfPlayers, setNumberOfPlayers] = useState("0");
     const [recentWinner, setRecentWinner] = useState("0");
     const [players, setPlayers] = useState([]);
-    // const [contractBalance, setContractBalance] = useState("0");
 
     /* View Functions */
 
@@ -66,7 +62,7 @@ export default function Info() {
             };
             res = await Moralis.executeFunction(options);
             setPlayers((players) => [...players, res]);
-        }        
+        }
     }
 
     useEffect(() => {
@@ -76,29 +72,35 @@ export default function Info() {
     }, [isWeb3Enabled]);
 
     return (
-        <div className="flex flex-col items-center p-5 bg-slate-600 " align="center">
+        <div className="flex flex-col items-center p-5" align="center">
             <div>
                 {deKinoAddress ? (
                     <>
-                        {/* <div>
-                            Entrance Fee: {ethers.utils.formatUnits(entranceFee, "ether")} ETH
-                        </div> */}
                         <div>Last winner was: {recentWinner}</div>
                         <div>The current number of players is: {numberOfPlayers}</div>
-
-                        <div className="bg-slate-300 flex-row sm:flex p-2" align="center">
-                            <div>Current entries:</div>
-                            <div>
-                                {players.map((user, index) => (
-                                    <div className=" bg-slate-100 m-4 p-4" key={index}>
-                                        Entry {index + 1}: {user}
-                                    </div>
-                                ))}
+                        {numberOfPlayers > 0 ? (
+                            <div
+                                className="bg-teal-500 flex-row p-2 border border-dashed rounded-lg border-teal-900 bg-teal-100"
+                                align="center"
+                            >
+                                <div align="center">Current entries:</div>
+                                <div>
+                                    {players.map((user, index) => (
+                                        <div
+                                            className=" bg-teal-200 border border-teal-800 rounded-md m-4 p-4"
+                                            key={index}
+                                        >
+                                            Entry {index + 1}: {user}
+                                        </div>
+                                    ))}
+                                </div>
                             </div>
-                        </div>
+                        ) : (
+                            <div></div>
+                        )}
                     </>
                 ) : (
-                    <div>Please connect to a supported chain </div>
+                    <div></div>
                 )}
             </div>
         </div>
--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -14,7 +14,7 @@ export default function LotteryEntrance() {
     // https://stackoverflow.com/questions/58252454/react-hooks-using-usestate-vs-just-variables
     const [entranceFee, setEntranceFee] = useState("0");
     const [numberOfPlayers, setNumberOfPlayers] = useState("0");
-    const [recentWinner, setRecentWinner] = useState("0");
+    const [deKinoState, setDeKinoState] = useState("0");
 
     const dispatch = useNotification();
 
@@ -34,7 +34,7 @@ export default function LotteryEntrance() {
 
     const { runContractFunction: getEntranceFee } = useWeb3Contract({
         abi: abi,
-        contractAddress: deKinoAddress, // specify the networkId
+        contractAddress: deKinoAddress,
         functionName: "getEntranceFee",
         params: {},
     });
@@ -46,10 +46,10 @@ export default function LotteryEntrance() {
         params: {},
     });
 
-    const { runContractFunction: getRecentWinner } = useWeb3Contract({
+    const { runContractFunction: getDeKinoState } = useWeb3Contract({
         abi: abi,
         contractAddress: deKinoAddress,
-        functionName: "getRecentWinner",
+        functionName: "getDeKinoState",
         params: {},
     });
 
@@ -62,10 +62,10 @@ export default function LotteryEntrance() {
         // })
         const entranceFeeFromCall = (await getEntranceFee()).toString();
         const numPlayersFromCall = (await getPlayersNumber()).toString();
-        const recentWinnerFromCall = await getRecentWinner();
+        const deKinoStateFromCall = await getDeKinoState();
         setEntranceFee(entranceFeeFromCall);
         setNumberOfPlayers(numPlayersFromCall);
-        setRecentWinner(recentWinnerFromCall);
+        setDeKinoState(deKinoStateFromCall);
     }
 
     useEffect(() => {
@@ -73,20 +73,8 @@ export default function LotteryEntrance() {
             updateUIValues();
         }
     }, [isWeb3Enabled]);
-    // no list means it'll update everytime anything changes or happens
-    // empty list means it'll run once after the initial rendering
-    // and dependencies mean it'll run whenever those things in the list change
 
-    // An example filter for listening for events, we will learn more on this next Front end lesson
-    // const filter = {
-    //     address: deKinoAddress,
-    //     topics: [
-    //         // the name of the event, parnetheses containing the data type of each event, no spaces
-    //         utils.id("DeKinoEnter(address)"),
-    //     ],
-    // }
-
-    // Probably could add some error handling
+    // TODO add some error handling
     const handleSuccess = async (tx) => {
         await tx.wait(1);
         updateUIValues();
@@ -104,14 +92,38 @@ export default function LotteryEntrance() {
     };
 
     return (
-        <div className="flex flex-col justify-center items-center p-5" align="center">
-            <h1 className="py-4 px-4 font-bold text-3xl">Lottery</h1>
+        <div className="flex flex-col justify-center items-center p-5 align-center" align="center">
+            <h1 className="text-gray-900 font-bold tracking-wider py-4 px-4 text-2xl">Welcome to DeKino Decentralized Lottery!</h1>
+            {!deKinoState ? (
+                <div>
+                    {numberOfPlayers > 0 ? (
+                        <div className="text-amber-400 font-bold p-12">
+                            <div className="text-lg">
+                                Lottery is Open! <p>Place your entries!</p>
+                            </div>
+                            <div className="text-sm text-gray-800 m-2">Next Lottery : {new Date().getHours() + 1}:00</div>
+                        </div>
+                    ) : (
+                        <div className="text-amber-400 font-bold p-12">No Active Lottery</div>
+                    )}
+                </div>
+            ) : (
+                <div className="m-12">
+                    <div className="text-amber-400 font-bold text-lg p-4 tracking-widest">
+                        Lottery is calculating!!!
+                    </div>
+                    <div className="text-sm">(Chainlink VRF)</div>
+                    <div className="text-amber-200 text-sm">
+                        No entries allowed at this moment...
+                    </div>
+                </div>
+            )}
             <div>
                 {deKinoAddress ? (
                     <>
                         <button
                             align="center"
-                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto"
+                            className="bg-lime-400 hover:bg-lime-500 text-brown border-2 border-lime-600 hover:border-lime-500 font-bold py-2 px-4 rounded-full ml-auto"
                             onClick={async () =>
                                 await enterDeKino({
                                     // onComplete:
@@ -125,7 +137,7 @@ export default function LotteryEntrance() {
                             {isLoading || isFetching ? (
                                 <div
                                     aligb="center"
-                                    className="animate-spin spinner-border h-8 w-8 border-b-2"
+                                    className="animate-spin h-8 w-8 border-b-2"
                                 ></div>
                             ) : (
                                 <div> Enter DeKino </div>
